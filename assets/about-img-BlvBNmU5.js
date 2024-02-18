927f056d9429d7dd45f4fa653a3e6a9082e3af77
J6749 <160389877+J6749@users.noreply.github.com>
2024-02-18 03:44:37 -0600

    Add files via upload

---
+++ assets/about-img-BlvBNmU5.js
@@ -0,0 +1,1 @@
+const s="/assets/about-img-CKVp2ynW.png";export{s as _};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/midmissourigoat/sw.js', { scope: '/midmissourigoat/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
